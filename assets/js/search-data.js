c3482826424bc85dbc6bbce998bf7c0b58e23805
nandobike <60858431+nandobike@users.noreply.github.com>
2025-12-15 07:04:28 +0000

    Deploying to gh-pages from @ nandobike/nandobike.github.io@cd6c883c41c58ac5f2113be9432c121981d339e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%76%62@%76%61%6C%6C%65%6A%6F%73.%63%6C", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/nandobike", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +516,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/nando-vallejos-cl", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-2262-9868", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +543,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=xD9OQosAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.vallejos.cl/", "_blank");
         },
       },{
       id: 'light-theme',
